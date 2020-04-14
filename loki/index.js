e6544a94f7ba624d5b51522b6bbcc33f16600e49
Harris R <harris.rothaermel@gmail.com>
2020-04-13 20:53:16 -0600

    Initial Completion

--- loki/index.js
+++ loki/index.js
@@ -1,12 +1,16 @@
-addEventListener('fetch', event => {
-  event.respondWith(handleRequest(event.request))
-})
+addEventListener("fetch", event => {
+  event.respondWith(handleRequest(event.request));
+});
 /**
  * Respond with hello worker text
  * @param {Request} request
  */
 async function handleRequest(request) {
-  return new Response('Hello worker!', {
-    headers: { 'content-type': 'text/plain' },
-  })
+  const URLS = await fetch(
+    "https://cfw-takehome.developers.workers.dev/api/variants"
+  )
+    .then(response => response.json())
+    .then(data => data.variants);
+
+  return fetch(URLS[Math.round(Math.random())]);
 }
